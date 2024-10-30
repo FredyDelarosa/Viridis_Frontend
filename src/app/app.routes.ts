3b05300c8fa879482add9bfa73dd7539e5d5b9eb
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2024-10-30 01:54:16 -0600

    Solicitud de Empresa a Administrador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router';
 //Users
 import { IloginComponent } from './components/users/ilogin/ilogin.component';
 import { IchoseRolComponent } from './components/users/ichose-rol/ichose-rol.component';
+//Admin
+import { IseeCompanyRequestComponent } from './components/admin/isee-company-request/isee-company-request.component';
 //Company
 import { IregisterCompanyComponent } from './components/company/iregister-company/iregister-company.component';
 import { IstatisticsComponent } from './components/company/istatistics/istatistics.component';
@@ -12,6 +14,7 @@ import { ImaterialsRequestComponent } from './components/company/imaterials-requ
 
 export const routes: Routes = [
     {path: "login", component: IloginComponent, pathMatch: "full"},
+    {path: "seecompany", component: IseeCompanyRequestComponent, pathMatch: "full"},
     {path: "registercompany", component: IregisterCompanyComponent, pathMatch:"full"},
     {path: "xs", component: IstatisticsComponent, pathMatch:"full"},
     {path: "materials", component: ImaterialsRequestComponent, pathMatch: "full"},
---
+++ src/app/components/admin/isee-company-request/isee-company-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-isee-company-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './isee-company-request.component.html',
+  styleUrl: './isee-company-request.component.scss'
+})
+export class IseeCompanyRequestComponent {
+
+}
