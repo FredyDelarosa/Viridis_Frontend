b8afd905fecc6778d999082c18cf1a0eac899e02
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2024-10-30 00:10:55 -0600

    Interfaz Estadisticas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 
 //Users
+import { IloginComponent } from './components/users/ilogin/ilogin.component';
 import { IchoseRolComponent } from './components/users/ichose-rol/ichose-rol.component';
 //Company
+import { IstatisticsComponent } from './components/company/istatistics/istatistics.component';
 import { ImaterialsRequestComponent } from './components/company/imaterials-request/imaterials-request.component';
 
 
 
 export const routes: Routes = [
+    {path: "login", component: IloginComponent, pathMatch: "full"},
+    {path: "xs", component: IstatisticsComponent, pathMatch:"full"},
     {path: "materials", component: ImaterialsRequestComponent, pathMatch: "full"},
     {path: "chose", component: IchoseRolComponent, pathMatch:"full"  },
 ];
