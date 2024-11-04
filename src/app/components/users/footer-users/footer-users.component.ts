e69856310e70c1502f0a98e6ae714ba4ba462cb2
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2024-11-03 23:21:58 -0600

    Creacion del Footer

---
+++ src/app/components/users/footer-users/footer-users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-users.component.html',
+  styleUrl: './footer-users.component.scss'
+})
+export class FooterUsersComponent {
+
+}
