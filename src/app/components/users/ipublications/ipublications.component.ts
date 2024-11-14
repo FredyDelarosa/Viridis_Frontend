2622c52c43e314913a1cafe4425e2ac026314533
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2024-11-14 03:31:44 -0600

    Creacion de la Interfaz Mis Publicaciones

---
+++ src/app/components/users/ipublications/ipublications.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+
+import { HeaderCompanyComponent } from '../../company/header-company/header-company.component';
+import { FooterUsersComponent } from '../footer-users/footer-users.component';
+import { DialogAddmaterialrequestComponent } from '../../company/materialrequest/dialog-addmaterialrequest/dialog-addmaterialrequest.component';
+
+
+import { MatDialog, } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-ipublications',
+  standalone: true,
+  imports: [HeaderCompanyComponent ,FooterUsersComponent, ],
+  templateUrl: './ipublications.component.html',
+  styleUrl: './ipublications.component.scss'
+})
+export class IpublicationsComponent {
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddmaterialrequestComponent, {
+      data: {},
+    });
+
+
+  }
+
+}
